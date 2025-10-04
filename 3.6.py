8b35b6dcbb58a77193576dcc1694f04be526240b
NeltarionSoul <khodkinok@gmail.com>
2025-10-03 18:04:07 -0700

    deleted

---
+++ 3.6.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+def kukold(x,y):
+    if len(x) != len(y):
+        print('idi n***i')
+    else:
+        a = len(x)*sum(x*) - sum(x)*sum(y)
+    print(a)
+r = np.random
+t = np.random
+kukold(r,t)
